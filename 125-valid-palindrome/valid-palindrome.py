3dee0be989d762d7987acae3f460f5afcc9a90a4
sarthakrana013 <83282709+sarthakrana013@users.noreply.github.com>
2026-03-11 18:37:32 +0530

    Time: 7 ms (81.38%) | Memory: 19.8 MB (41.44%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,25 +1,18 @@
 class Solution:
-    def alpha(self,s,i):
-        x=ord(s[i])
-        if 97<=x<=122 or 48<=x<=57:
-            return True
-        else:
-            return False
     def isPalindrome(self, s: str) -> bool:
-        s=s.lower()
-        n=len(s)
-        i,j=0,n-1
+        s = s.lower()
+        i, j = 0, len(s) - 1
 
-        while i<j:
-            if not self.alpha(s,i):
-                i+=1
-                continue
-            if not self.alpha(s,j):
-                j-=1
-                continue
-            if s[i]==s[j]:
-                i+=1
-                j-=1
-            else:
+        while i < j:
+            while i < j and not s[i].isalnum():
+                i += 1
+            while i < j and not s[j].isalnum():
+                j -= 1
+
+            if s[i] != s[j]:
                 return False
+
+            i += 1
+            j -= 1
+
         return True
